52c5264f42b2b378d8f1b02b704de80175b845e6
Keshav Agarwal <99586399+agkeshav@users.noreply.github.com>
2023-09-26 13:30:17 +0530

    Admin Form added

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,4 +1,43 @@
+import "./Admin.css";
 const Admin = () => {
-  return <div>Admin</div>
-}
-export default Admin
+  return (
+    <>
+      <section className="card">
+        <div className="container mt-5 pt-4 pb-2">
+          <div className="row" style={{ justifyContent: "center" }}>
+            <div className="col-sm-5  col-md-6 offset-md-0 contact">
+              <h2>Admin Login</h2>
+              <form action="complaint.php" name="contactform" method="post">
+                <div className="form-group">
+                  <label for="email">Email</label>
+                  <input
+                    className="form-control"
+                    type="email"
+                    name="email"
+                    placeholder="Your Email..."
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label for="password">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    placeholder="Enter Your Password..."
+                    required
+                  />
+                </div>
+
+                <button type="submit" className="btn btn-outline-success mt-2">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default Admin;
